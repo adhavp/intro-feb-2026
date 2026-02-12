75bc20a2dd55ff921323fc54649b973e44f20440
Adhav Param <adhavparam3@gmail.com>
2026-02-12 16:14:16 -0500

    calculator update

---
+++ src/StringCalculatorKata Thursday/StringCalculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿
+using NSubstitute.Core;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+        else
+        {
+            string[] nums = numbers.Split(new[] { ',', '\n' });
+            int sum = 0;
+            foreach(var num in nums)
+            {
+                sum += int.Parse(num);
+            }
+            return sum;
+        }
+        /*
+        else if (numbers.Contains(","))
+        {
+            string[] nums = numbers.Split(',');
+            int sum = 0;
+            foreach (var num in nums)
+            {
+                sum += int.Parse(num);
+            }
+            return sum;
+        }
+        else if (int.TryParse(numbers, out int result))
+        {
+            return result;
+        }
+            return -42;
+        */
+    }
+}
